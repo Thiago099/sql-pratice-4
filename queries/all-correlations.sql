941d5d26b006a602bf535d47cbf6da58e35fdf02
Thiago099 <tecnologiasthiago@gmail.com>
2022-06-02 20:09:26 -0300

    [fix] query

--- queries/all-correlations.sql
+++ queries/all-correlations.sql
@@ -29,12 +29,12 @@ INNER JOIN object_name ON object_name.id_object = generalization.id_object_from
 SELECT 
 object_correlation.id,
 `correlation`.`name` AS `correlation`,
-IFNULL(`object_from_name`.`name`, IFNULL(CONCAT((SELECT alias FROM object_alias WHERE id_object = object_from.id),' ',object_from.id),object_from.id)) AS `object_from`,
-IFNULL(`object_to_name`.`name`, IFNULL(CONCAT((SELECT alias FROM object_alias WHERE id_object = object_to.id),' ',object_to.id),object_to.id)) AS `object_to`
+IFNULL(CONCAT(`object_from_name`.`name`,' ',object_from.id), IFNULL(CONCAT((SELECT alias FROM object_alias WHERE id_object = object_from.id),' ',object_from.id),object_from.id)) AS `object_from`,
+IFNULL(CONCAT(`object_to_name`.`name`,' ',object_to.id), IFNULL(CONCAT((SELECT alias FROM object_alias WHERE id_object = object_to.id),' ',object_to.id),object_to.id)) AS `object_to`
 FROM `object_correlation`
 LEFT JOIN `correlation` ON `correlation`.`id` = `object_correlation`.`id_correlation`
 LEFT JOIN `object` AS `object_from` ON `object_from`.`id` = `object_correlation`.`id_object_from`
 LEFT JOIN `object_name` AS `object_from_name` ON `object_from_name`.`id_object` = `object_from`.`id`
 LEFT JOIN `object` AS `object_to` ON `object_to`.`id` = `object_correlation`.`id_object_to`
 LEFT JOIN `object_name` AS `object_to_name` ON `object_to_name`.`id_object` = `object_to`.`id`
-ORDER BY correlation
+ORDER BY correlation, object_from, object_to
